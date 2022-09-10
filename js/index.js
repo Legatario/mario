b2f151ce13ccf19b81f961bfd44a1f00b49084f6
Luis Phillipe <l.phillipe.d@gmail.com>
2022-09-09 21:55:56 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,46 @@
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe');
+const score = document.querySelector('.score');
+const restart = document.querySelector('.reset');
+
+function jump(){
+    mario.classList.add('jump');
+
+    setInterval(()=>{
+        mario.classList.remove('jump')
+    }, 700);
+}
+
+let dotScore = 0;
+game()
+
+function game(){
+    const loop = setInterval(()=>{
+        const pipePostion = pipe.offsetLeft;
+        const marioPostion = +window.getComputedStyle(mario).bottom.replace('px','');
+        
+        if(pipePostion <= 120 && pipePostion > 0 && marioPostion < 80){
+            pipe.style.animation='none'
+            pipe.style.left = `${pipePostion}px`
+            
+            mario.style.animation='none'
+            mario.src = './imagem/game-over.png'
+            mario.style.width = '75px'
+            mario.style.marginLeft = '50px'
+            mario.style.bottom = `${marioPostion}px`
+            
+            clearInterval(loop);
+        }if(pipePostion <= 10 && pipePostion > 0 && marioPostion > 80){
+            dotScore += 500
+            score.innerHTML = dotScore
+        }
+    }, 10)
+
+}
+
+// document.addEventListener('keydown', jump);
+
+restart.addEventListener('click',()=>{
+    document.location.reload(true);
+    
+})
